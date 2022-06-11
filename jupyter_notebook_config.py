a8b9c8eb6f70637a6e1ad10529df23458219c269
vantran89 <84663422+vantran89@users.noreply.github.com>
2022-06-11 22:41:37 +0200

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,2 +1,2 @@
-c.NotebookApp.contents_manager_class="jupytext.TextFileContentsManager"
+# c.NotebookApp.contents_manager_class="jupytext.TextFileContentsManager"
 c.ContentsManager.default_jupytext_formats = ".ipynb,.Rmd"
